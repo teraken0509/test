82daaa08f23190f36143dd8acf3c42a299719658
Nunokawa <nunokawa219@i.softbank.jp>
2020-12-11 12:19:11 +0000

    3

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,28 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{-- layouts/profile.blade.phpを読み込む --}}
-    @extends('layouts.profile')
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
-    {{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-    @section('title', 'プロフィールの新規作成')
+{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'プロフィールの新規作成')
 
-    {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
-    @section('content')
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 mx-auto">
-                    <h2>プロフィール新規作成</h2>
-                </div>
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
             </div>
         </div>
-    @endsection
-        <title>Myprofile</title>
-    </head>
-    <body>
-        <h1>Myプロフィール作成画面</h1>
-    </body>
-</html>
+    </div>
+@endsection
