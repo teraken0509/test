e399fbfcef5e2a3811a83b4827b96084d716fa71
Nunokawa <nunokawa219@i.softbank.jp>
2020-12-11 10:16:49 +0000

    3

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -4,7 +4,22 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- layouts/profile.blade.phpを読み込む --}}
+    @extends('layouts.profile')
 
+    {{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+    @section('title', 'プロフィールの新規作成')
+
+    {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+    @section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h2>プロフィール新規作成</h2>
+                </div>
+            </div>
+        </div>
+    @endsection
         <title>Myprofile</title>
     </head>
     <body>
