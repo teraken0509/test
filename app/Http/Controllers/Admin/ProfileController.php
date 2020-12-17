768e4bb8efcb95fd594dd25c8ebf8db2f0c75a6b
Nunokawa <nunokawa219@i.softbank.jp>
2020-12-17 11:08:37 +0000

    8

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -22,17 +22,19 @@ class ProfileController extends Controller
         $form = $request->all();
         
         // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+        /*
         if (isset($form['image'])) {
             $path = $request->file('image')->store('public/image');
             $profile->image_path = basename($path);
         } else {
             $profile->image_path = null;
         }
+        */
         
         // フォームから送信されてきた_tokenを削除する
-        unset($form['_token']);
+        //unset($form['_token']);
         // フォームから送信されてきたimageを削除する
-        unset($form['image']);
+        //unset($form['image']);
         
         // データベースに保存する
         $profile->fill($form);
