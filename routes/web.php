93b0bcd785fe7db7426338941e500985d7e041be
Nunokawa <nunokawa219@i.softbank.jp>
2021-01-04 14:47:48 +0000

    11

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,8 @@ Route::group(['prefix' => 'admin/profile', 'middleware' => 'auth'], function() {
     Route::post('create', 'Admin\ProfileController@create');
     Route::get('edit', 'Admin\ProfileController@edit');
     Route::post('edit', 'Admin\ProfileController@update');
-    
 });
 
 Auth::routes();
+Route::get('/', 'NewsController@index');
+Route::get('/profile', 'ProfileController@index');
